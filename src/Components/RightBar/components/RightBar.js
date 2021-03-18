b481dcc6e0d5c7cd472696c05229592723b67875
arssnvsk <arssnvsk@gmail.com>
2021-03-18 14:49:49 +0200

    Artem commit 18.03.2021

--- src/Components/RightBar/components/RightBar.js
+++ src/Components/RightBar/components/RightBar.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import '../css_modules/rightBarCssModule.css'
 import pet from '../Images/petAvatar.png'
-import {add_1, add_2} from "../../../utils/constants/constants";
+// import {add_1, add_2} from "../../../utils/constants/constants";
 
 const RightBar = () => {
     return (
 
 
+        <div className="container">
+            <div className="main">
 
-
-
-<div className="grid-container">
-<div className="main">
-
-    <div className='add2'>
-        <img src={add_2} alt={'add_2'}/>
-    </div>
+                <div className='add2'>
+                    <img src={add_2} alt={'add_2'}/>
+                </div>
             </div>
         </div>
     )
