7fe7430b423cd9d7a3784e9d0674b47150a265a9
arssnvsk <arssnvsk@gmail.com>
2021-03-24 09:56:43 +0200

    Artem commit 24.03.2021

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React, {useEffect} from 'react'
 import './App.css';
 import Menu from "./Components/Menu/components/Menu";
 import Guest from "./Components/Guest/components/Guest";
-import {Switch, Route, useHistory} from 'react-router-dom'
+import {Switch, Route, useHistory, Redirect} from 'react-router-dom';
 import {
-    FAVORITES, FOSTERING,
+    FAVORITES,
+    FOSTERING,
     FOUND_PAGE,
     HOME_PAGE,
     HOTELS,
     LOST_PAGE,
     USER_PAGE,
-    VET_HELP, WALKING
+    VET_HELP,
+    WALKING,
 } from "./utils/constants/constants";
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 import {loginUser} from './Redux/actions/accountingActions'
-import {accountingReducer} from "./Redux/reducers/accountingReducer";
 
 
 function App({loginUser, token}) {
@@ -37,23 +38,19 @@ function App({loginUser, token}) {
     return (
 
         <Switch>
-            <Route exact path={`/${HOME_PAGE}`}><Menu page={HOME_PAGE}/></Route>
-            <Route exact path={`/${FOUND_PAGE}`}><Menu page={FOUND_PAGE}/></Route>
-            <Route exact path={`/${LOST_PAGE}`}><Menu page={LOST_PAGE}/></Route>
-            <Route exact path={`/${VET_HELP}`}><Menu page={VET_HELP}/></Route>
-            <Route exact path={`/${HOTELS}`}><Menu page={HOTELS}/></Route>
-            <Route exact path={`/${FOSTERING}`}><Menu page={FOSTERING}/></Route>
-            <Route exact path={`/${WALKING}`}><Menu page={WALKING}/></Route>
-            <Route exact path={`/${USER_PAGE}`}><Menu page={USER_PAGE}/></Route>
-            <Route exact path={`/${FAVORITES}`}><Menu page={FAVORITES}/></Route>
+            <Route exact path={`/${HOME_PAGE}`}>{token ? <Menu page={HOME_PAGE}/> : <Redirect to={'/'}/>}</Route>
+            <Route exact path={`/${FOUND_PAGE}`}>{token ? <Menu page={FOUND_PAGE}/> : <Redirect to={'/'}/>}</Route>
+            <Route exact path={`/${LOST_PAGE}`}>{token ? <Menu page={LOST_PAGE}/> : <Redirect to={'/'}/>}</Route>
+            <Route exact path={`/${VET_HELP}`}>{token ? <Menu page={VET_HELP}/> : <Redirect to={'/'}/>}</Route>
+            <Route exact path={`/${HOTELS}`}>{token ? <Menu page={HOTELS}/> : <Redirect to={'/'}/>}</Route>
+            <Route exact path={`/${FOSTERING}`}>{token ? <Menu page={FOSTERING}/> : <Redirect to={'/'}/>}</Route>
+            <Route exact path={`/${WALKING}`}>{token ? <Menu page={WALKING}/> : <Redirect to={'/'}/>}</Route>
+            <Route exact path={`/${USER_PAGE}`}>{token ? <Menu page={USER_PAGE}/> : <Redirect to={'/'}/>}</Route>
+            <Route exact path={`/${FAVORITES}`}>{token ? <Menu page={FAVORITES}/> : <Redirect to={'/'}/>}</Route>
             <Route exact path={['/', '/guest']}><Guest/></Route>
             <Route><h1>Error. No such page...</h1></Route>
         </Switch>
 
-        // <Switch>
-        //     {token ? <Route exact path={`/${HOME_PAGE}`}><Menu page={HOME_PAGE}/></Route>
-        //         : <Route exact path={['/', '/guest']}><Guest/></Route>}
-        // </Switch>
     );
 }
 
