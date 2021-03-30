50e75b77e02d2ba948e7cd024877b6d053658d6f
davidonda1 <miasnikov89@gmail.com>
2021-03-30 16:42:00 +0300

    David found_post_hooks

--- src/Components/Posts/components/Found_Post.jsx
+++ src/Components/Posts/components/Found_Post.jsx
@@ -8,12 +8,17 @@ import {FaFileUpload} from "react-icons/fa";
 import user_avatar from '../../../utils/Images/user_avatar.png'
 import {AiOutlineArrowDown} from "react-icons/all";
 import {GiPuppet} from "react-icons/gi";
+import {useState} from "react/cjs/react.production.min";
 
 // import {FOUND_POST_TEXT} from "../../utils/constants/constants";
 
 
 const Found_Post = () => {
 
+    const [object, setObject] = useState({});
+    const handleClickInfo = (event, field) => {
+        setObject((prevState) => ({...prevState, [field]: event.target.value}));
+    }
     return (
         <div className='container'>
             <div className='container'>
@@ -29,82 +34,80 @@ const Found_Post = () => {
                 <textarea className='textarea h col-1  offset-6  fixed-top '>img1.jpg X </textarea>
                 <div className='row col-6 label_select'>
                     <label className='mr-3'>Type:</label>
-                    <select className='select mb-2'>
+                    <select onChange={(event)=>handleClickInfo(event,'type')} className='select mb-2'>
                         <option>Dog</option>
                         <option>Cat</option>
-                    </select>
-                    <AiOutlineArrowDown class='ml-3 mt-1'/>
-                </div>
-                <div className='row col-6 label_select'>
-                    <label className='mr-2'>Sex:</label>
-                    <select className='select mb-2 ml-2'>
+                        </select>
+                        <AiOutlineArrowDown class='ml-3 mt-1'/>
+                        </div>
+                        <div className='row col-6 label_select'>
+                        <label className='mr-2'>Sex:</label>
+                        <select onChange={(event)=>handleClickInfo(event,'sex')}  className='select mb-2 ml-2'>
                         <option>Male</option>
                         <option>Female</option>
-                    </select>
-                    <AiOutlineArrowDown class='ml-1 mt-1'/>
-                </div>
-                <div className='row col-6 label_input'>
-                    <label>Breed:</label>
-                    <input className='input ml-3 mb-1' type='text' placeholder='Golden Retriever'/>
-                </div>
-                <div className='row col-6 label_input'>
-                    <label>Color:</label>
-                    <input className='input ml-3 mb-1' type='text' placeholder='Beige'/>
-                </div>
-                <div className='row col-6 label_select'>
-                    <label>Height:</label>
-                    <select className='select mb-2 ml-2'>
+                        </select>
+                        <AiOutlineArrowDown class='ml-1 mt-1'/>
+                        </div>
+                        <div className='row col-6 label_input'>
+                        <label>Breed:</label>
+                        <input onChange={(event)=>handleClickInfo(event,'breed')} className='input ml-3 mb-1' type='text' placeholder='Golden Retriever'/>
+                        </div>
+                        <div className='row col-6 label_input'>
+                        <label>Color:</label>
+                        <input onChange={(event)=>handleClickInfo(event,'color')} className='input ml-3 mb-1' type='text' placeholder='Beige'/>
+                        </div>
+                        <div className='row col-6 label_select'>
+                        <label>Height:</label>
+                        <select onChange={(event)=>handleClickInfo(event,'height')} className='select mb-2 ml-2'>
                         <option>45-70 cm</option>
-                    </select>
-                    <AiOutlineArrowDown class='mt-1'/>
-                </div>
-                <div className='row   '>
-                    <label className='col-3 '>Distinktive features: <span className='green_small'></span></label>
-                    <textarea className='textarea '>blue collar with stars, no left ear, damaged tail.</textarea>
-                </div>
-
-
-                <div className='row mt-2 '>
-                    <label className='col-3 '>Description: <span
+                        </select>
+                        <AiOutlineArrowDown class='mt-1'/>
+                        </div>
+                        <div className='row   '>
+                        <label className='col-3 '>Distinktive features: <span className='green_small'></span></label>
+                        <textarea onChange={(event)=>handleClickInfo(event,'distinktive_features')} className='textarea '>blue collar with stars, no left ear, damaged tail.</textarea>
+                        </div>
+                        <div className='row mt-2 '>
+                        <label className='col-3 '>Description: <span
                         className='green_small'></span></label>
-                    <textarea className='textarea_1 '>brown fox jumps over a lazy dog. DJs flock by when jhkjk jhgMTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs.</textarea>
+                        <textarea onChange={(event)=>handleClickInfo(event,'description')}   className='textarea_1 '>brown fox jumps over a lazy dog. DJs flock by when jhkjk jhgMTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs.</textarea>
 
-                </div>
-                <div className='row mt-2  '>
-                    <label className='col-3 '>Location:</label>
-                    <textarea className='textarea'>Florentin Street, Tel Aviv</textarea>
-                    <div>
+                        </div>
+                        <div className='row mt-2  '>
+                        <label className='col-3 '>Location:</label>
+                        <textarea onChange={(event)=>handleClickInfo(event,'location')} className='textarea'>Florentin Street, Tel Aviv</textarea>
+                        <div>
                         <p className='col-6 offset-7 DRAG '><FaFileUpload/>Drag and drop photos or</p>
                         <button className='col-6 offset-7 BTN_around justify-content-center '><p
-                            className='BTN justify-content-center'>Browse</p>
+                        className='BTN justify-content-center'>Browse</p>
                         </button>
-                    </div>
-                </div>
-                <div className='container mt-3'>
-                    <div className='row mr-3'>
+                        </div>
+                        </div>
+                        <div className='container mt-3'>
+                        <div className='row mr-3'>
                         <label className='col-3'>Contacts:</label>
-                        <input className='input' type='text' placeholder='Phone*'/>
-                        <input className='input ml-2' type='text' placeholder='Email'/>
-                        <input className='input ml-2' type='text' placeholder='Facebook profile'/>
+                        <input onChange={(event)=>handleClickInfo(event,'phone')} className='input' type='text' placeholder='Phone*'/>
+                        <input onChange={(event)=>handleClickInfo(event,'email')} className='input ml-2' type='text' placeholder='Email'/>
+                        <input onChange={(event)=>handleClickInfo(event,'facebook_profile')} className='input ml-2' type='text' placeholder='Facebook profile'/>
 
-                    </div>
-                </div>
-                <div className='container mt-3'>
-                    <div className='row mr-3'>
+                        </div>
+                        </div>
+                        <div className='container mt-3'>
+                        <div className='row mr-3'>
                         <img className='img' src={user_avatar}/>
-                     <p className='HEADER_TEXT col-3 ml-2 mt-3'>John Goodboi</p>
+                        <p className='HEADER_TEXT col-3 ml-2 mt-3'>John Goodboi</p>
                         <button className='col-2 offset-10 my_btn_around justify-content-center '><p
-                            className='BTN_1 justify-content-center '>Publish</p>
+                        className='BTN_1 justify-content-center '>Publish</p>
                         </button>
 
-                    </div>
-                </div>
-            </div>
-            {/* <div className='container'>
+                        </div>
+                        </div>
+                        </div>
+                    {/* <div className='container'>
 
             </div>*/}
-        </div>
-    );
-};
+                        </div>
+                        );
+                    };
 
 export default Found_Post;
