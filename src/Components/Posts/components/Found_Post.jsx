4d767a8e10ca665c5a26f5fea3fd6946a92e439e
davidonda1 <miasnikov89@gmail.com>
2021-03-30 15:37:59 +0300

    David found_post

--- src/Components/Posts/components/Found_Post.jsx
+++ src/Components/Posts/components/Found_Post.jsx
@@ -1,127 +1,108 @@
 import React from 'react';
 import '../css_modules/find_lost_post.css'
+import man_with_pet from '../../../utils/Images/manWithPetImg.svg'
+import import_1 from '../../../utils/Images/question_1.svg'
 import LeftBar from "../../Left_Bar/components/LeftBar";
 import {FaPaw} from "react-icons/fa";
 import {FaFileUpload} from "react-icons/fa";
 import user_avatar from '../../../utils/Images/user_avatar.png'
+import {AiOutlineArrowDown} from "react-icons/all";
 import {GiPuppet} from "react-icons/gi";
+
 // import {FOUND_POST_TEXT} from "../../utils/constants/constants";
 
 
 const Found_Post = () => {
+
     return (
-        <div className="grid-container">
-           {/* <div className="header">
-                FIXME
-                <Header/>
-            </div>*/}
-          {/*  <div className="right_bar">
-                <LeftBar/>
-                FIXME
-                <RightBar/>
-            </div>*/}
-           {/* <div className="left_bar">
-               <LeftBar/>
-            </div>*/}
-            <div className="upper_text">
-                <p><span className='upper_text_span'>Found a pet?</span> Cool!</p>
+        <div className='container'>
+            <div className='container'>
+                <div className='row'>
+                    <div className=' col-12'>
+                        <p className='HEADER_TEXT'>Found a pet? <span
+                            className='pSpan'>Please complete the form to help.</span></p>
+                    </div>
+                </div>
             </div>
-            <div className="main"></div>
-            <div className="type">
-                <label>Type: </label>
+            <div className='container '>
+                <img className='fixed-top offset-5 img_man  ' src={man_with_pet}/>
+                <textarea className='textarea h col-1  offset-6  fixed-top '>img1.jpg X </textarea>
+                <div className='row col-6 label_select'>
+                    <label className='mr-3'>Type:</label>
+                    <select className='select mb-2'>
+                        <option>Dog</option>
+                        <option>Cat</option>
+                    </select>
+                    <AiOutlineArrowDown class='ml-3 mt-1'/>
+                </div>
+                <div className='row col-6 label_select'>
+                    <label className='mr-2'>Sex:</label>
+                    <select className='select mb-2 ml-2'>
+                        <option>Male</option>
+                        <option>Female</option>
+                    </select>
+                    <AiOutlineArrowDown class='ml-1 mt-1'/>
+                </div>
+                <div className='row col-6 label_input'>
+                    <label>Breed:</label>
+                    <input className='input ml-3 mb-1' type='text' placeholder='Golden Retriever'/>
+                </div>
+                <div className='row col-6 label_input'>
+                    <label>Color:</label>
+                    <input className='input ml-3 mb-1' type='text' placeholder='Beige'/>
+                </div>
+                <div className='row col-6 label_select'>
+                    <label>Height:</label>
+                    <select className='select mb-2 ml-2'>
+                        <option>45-70 cm</option>
+                    </select>
+                    <AiOutlineArrowDown class='mt-1'/>
+                </div>
+                <div className='row   '>
+                    <label className='col-3 '>Distinktive features: <span className='green_small'></span></label>
+                    <textarea className='textarea '>blue collar with stars, no left ear, damaged tail.</textarea>
+                </div>
 
-            </div>
-            <div className="dog">
-                <select>
-                    <option>Dog</option>
-                </select>
-            </div>
-            <div className="sex">
-                <label>Sex</label>
-            </div>
-            <div className="male">
-                <select>
-                    <option>Male</option>
-                    <option>Female</option>
-                </select>
-            </div>
-            <div className="breed">
-                <label>Breed: </label>
-            </div>
-            <div className="golden_retriever">
-                <input type='text' placeholder='Golden Retriever'/>
-            </div>
-            <div className="color">
-                <label>Color: </label>
-                <div><label>Height:</label></div>
-            </div>
-            <div className="white">
-                <input type='text' placeholder='Beige'/>
-                <select className='height_select'>
-                    <option>45-70cm</option>
-                    <option>very very big dog... be careful</option>
-                </select>
-            </div>
-            <div className="features">
-                <label>Distinctive features: </label>
-            </div>
-            <div className="features_area">
-                <textarea placeholder='blue collar with stars, no left ear, damaged tail.'/>
-            </div>
-            <div className="man_dog">
-                <GiPuppet/>
-            </div>
-            <div className="description">
-                <label>Description: </label>
-            </div>
-            <div className="description_area">
-                <textarea
-                    placeholder='brown fox jumps over a lazy dog. DJs flock by when jhkjk jhgMTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs.'/>
-            </div>
-            <div className="files">
 
+                <div className='row mt-2 '>
+                    <label className='col-3 '>Description: <span
+                        className='green_small'></span></label>
+                    <textarea className='textarea_1 '>brown fox jumps over a lazy dog. DJs flock by when jhkjk jhgMTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs.</textarea>
+
+                </div>
+                <div className='row mt-2  '>
+                    <label className='col-3 '>Location:</label>
+                    <textarea className='textarea'>Florentin Street, Tel Aviv</textarea>
+                    <div>
+                        <p className='col-6 offset-7 DRAG '><FaFileUpload/>Drag and drop photos or</p>
+                        <button className='col-6 offset-7 BTN_around justify-content-center '><p
+                            className='BTN justify-content-center'>Browse</p>
+                        </button>
+                    </div>
+                </div>
+                <div className='container mt-3'>
+                    <div className='row mr-3'>
+                        <label className='col-3'>Contacts:</label>
+                        <input className='input' type='text' placeholder='Phone*'/>
+                        <input className='input ml-2' type='text' placeholder='Email'/>
+                        <input className='input ml-2' type='text' placeholder='Facebook profile'/>
+
+                    </div>
+                </div>
+                <div className='container mt-3'>
+                    <div className='row mr-3'>
+                        <img className='img' src={user_avatar}/>
+                     <p className='HEADER_TEXT col-3 ml-2 mt-3'>John Goodboi</p>
+                        <button className='col-2 offset-10 my_btn_around justify-content-center '><p
+                            className='BTN_1 justify-content-center '>Publish</p>
+                        </button>
+
+                    </div>
+                </div>
             </div>
-            <div className="drag-and-drop">
-                {/*<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"*/}
-                {/*     className="bi bi-upload" viewBox="0 0 16 16">*/}
-                {/*    <path*/}
-                {/*        d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>*/}
-                {/*    <path*/}
-                {/*        d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/>*/}
-                {/*</svg>*/}
-                <FaFileUpload/>
-                <p>Drag and drop photos or</p>
-                <button className='drag-and-drop-btn'>Browse</button>
-            </div>
-            <div className="location">
-                <label>Location: </label>
-            </div>
-            <div className="location_area">
-                <input placeholder='Florentin Street, Tel Aviv' type='text'/>
-            </div>
-            <div className="browse">
-            </div>
-            <div className="contacts">
-                <label>Contacts: </label>
-            </div>
-            <div className="phone">
-                <input type='text' placeholder='Phone*'/>
-            </div>
-            <div className="email">
-                <input type='email' placeholder='Email'/>
-            </div>
-            <div className="facebook">
-                <input type='text' placeholder='Facebook EditUser'/>
-            </div>
-            <div className="user_avatar">
-                <img src={user_avatar} alt='user_avatar'/>
-            </div>
-            <div className="user_name">
-                <p>{0 || 'John Goodboi'}</p>
-            </div>
-            <div className="publish">
-                <p><FaPaw/> Publish</p>
-            </div>
+            {/* <div className='container'>
+
+            </div>*/}
         </div>
     );
 };
